023fa0328a9ad37ab201a82521f46bb751f952f1
jlevine272 <53155851+jlevine272@users.noreply.github.com>
2023-05-07 19:15:22 -0500

    Refactored and wrote README.

--- segment_proj.py
+++ segment_proj.py
@@ -25,16 +25,12 @@ from torch.autograd import Variable
 
 import drn
 import data_transforms as transforms
+from segment import DRNSeg
 
 try:
     from modules import batchnormsync
 except ImportError:
     pass
-
-FORMAT = "[%(asctime)-15s %(filename)s:%(lineno)d %(funcName)s] %(message)s"
-logging.basicConfig(format=FORMAT)
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 
 CITYSCAPE_PALETTE = np.asarray([
@@ -60,103 +56,6 @@ CITYSCAPE_PALETTE = np.asarray([
     [0, 0, 0]], dtype=np.uint8)
 
 
-TRIPLET_PALETTE = np.asarray([
-    [0, 0, 0, 255],
-    [217, 83, 79, 255],
-    [91, 192, 222, 255]], dtype=np.uint8)
-
-
-def fill_up_weights(up):
-    w = up.weight.data
-    f = math.ceil(w.size(2) / 2)
-    c = (2 * f - 1 - f % 2) / (2. * f)
-    for i in range(w.size(2)):
-        for j in range(w.size(3)):
-            w[0, 0, i, j] = \
-                (1 - math.fabs(i / f - c)) * (1 - math.fabs(j / f - c))
-    for c in range(1, w.size(0)):
-        w[c, 0, :, :] = w[0, 0, :, :]
-
-
-class DRNSeg(nn.Module):
-    def __init__(self, model_name, classes, pretrained_model=None,
-                 pretrained=True, use_torch_up=False):
-        super(DRNSeg, self).__init__()
-        model = drn.__dict__.get(model_name)(
-            pretrained=pretrained, num_classes=1000)
-        pmodel = nn.DataParallel(model)
-        if pretrained_model is not None:
-            pmodel.load_state_dict(pretrained_model)
-        self.base = nn.Sequential(*list(model.children())[:-2])
-
-        self.seg = nn.Conv2d(model.out_dim, classes,
-                             kernel_size=1, bias=True)
-        self.softmax = nn.LogSoftmax()
-        m = self.seg
-        n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-        m.weight.data.normal_(0, math.sqrt(2. / n))
-        m.bias.data.zero_()
-        if use_torch_up:
-            self.up = nn.UpsamplingBilinear2d(scale_factor=8)
-        else:
-            up = nn.ConvTranspose2d(classes, classes, 16, stride=8, padding=4,
-                                    output_padding=0, groups=classes,
-                                    bias=False)
-            fill_up_weights(up)
-            up.weight.requires_grad = False
-            self.up = up
-
-    def forward(self, x):
-        x = self.base(x)
-        x = self.seg(x)
-        y = self.up(x)
-        return self.softmax(y), x
-
-    def optim_parameters(self, memo=None):
-        for param in self.base.parameters():
-            yield param
-        for param in self.seg.parameters():
-            yield param
-
-
-class SegList(torch.utils.data.Dataset):
-    def __init__(self, data_dir, phase, transforms, list_dir=None,
-                 out_name=False):
-        self.list_dir = data_dir if list_dir is None else list_dir
-        self.data_dir = data_dir
-        self.out_name = out_name
-        self.phase = phase
-        self.transforms = transforms
-        self.image_list = None
-        self.label_list = None
-        self.bbox_list = None
-        self.read_lists()
-
-    def __getitem__(self, index):
-        data = [Image.open(join(self.data_dir, self.image_list[index]))]
-        if self.label_list is not None:
-            data.append(Image.open(
-                join(self.data_dir, self.label_list[index])))
-        data = list(self.transforms(*data))
-        if self.out_name:
-            if self.label_list is None:
-                data.append(data[0][0, :, :])
-            data.append(self.image_list[index])
-        return tuple(data)
-
-    def __len__(self):
-        return len(self.image_list)
-
-    def read_lists(self):
-        image_path = join(self.list_dir, self.phase + '_images.txt')
-        label_path = join(self.list_dir, self.phase + '_labels.txt')
-        assert exists(image_path)
-        self.image_list = [line.strip() for line in open(image_path, 'r')]
-        if exists(label_path):
-            self.label_list = [line.strip() for line in open(label_path, 'r')]
-            assert len(self.image_list) == len(self.label_list)
-
-
 class AverageMeter(object):
     """Computes and stores the average and current value"""
     def __init__(self):
@@ -175,54 +74,6 @@ class AverageMeter(object):
         self.avg = self.sum / self.count
 
 
-def accuracy(output, target):
-    """Computes the precision@k for the specified values of k"""
-    # batch_size = target.size(0) * target.size(1) * target.size(2)
-    _, pred = output.max(1)
-    pred = pred.view(1, -1)
-    target = target.view(1, -1)
-    correct = pred.eq(target)
-    correct = correct[target != 255]
-    correct = correct.view(-1)
-    score = correct.float().sum(0).mul(100.0 / correct.size(0))
-    return score.data[0]
-def fast_hist(pred, label, n):
-    k = (label >= 0) & (label < n)
-    return np.bincount(
-        n * label[k].astype(int) + pred[k], minlength=n ** 2).reshape(n, n)
-
-
-def per_class_iu(hist):
-    return np.diag(hist) / (hist.sum(1) + hist.sum(0) - np.diag(hist))
-
-
-def save_output_images(predictions, filenames, output_dir):
-    """
-    Saves a given (B x C x H x W) into an image file.
-    If given a mini-batch tensor, will save the tensor as a grid of images.
-    """
-    # pdb.set_trace()
-    for ind in range(len(filenames)):
-        im = Image.fromarray(predictions[ind].astype(np.uint8))
-        fn = os.path.join(output_dir, filenames[ind][:-4] + '.png')
-        out_dir = split(fn)[0]
-        if not exists(out_dir):
-            os.makedirs(out_dir)
-        im.save(fn)
-
-
-def save_colorful_images(predictions, filenames, output_dir, palettes):
-   """
-   Saves a given (B x C x H x W) into an image file.
-   If given a mini-batch tensor, will save the tensor as a grid of images.
-   """
-   for ind in range(len(filenames)):
-       im = Image.fromarray(palettes[predictions[ind].squeeze()])
-       fn = os.path.join(output_dir, filenames[ind][:-4] + '.png')
-       out_dir = split(fn)[0]
-       if not exists(out_dir):
-           os.makedirs(out_dir)
-       im.save(fn)
 
 def generate_colorful_images(predictions):
     return [ Image.fromarray(CITYSCAPE_PALETTE[predictions[i].squeeze()]) for i in range(len(predictions)) ]
@@ -231,16 +82,11 @@ def generate_road_mask(predictions):
     return [Image.fromarray(predictions[i].squeeze() == 0) for i in range(len(predictions))]
 
 
-def test(eval_data_loader, model, num_classes,
-         output_dir='pred', has_gt=False, save_vis=False, output_ims=True):
+def semantic_segmentation(eval_data_loader, model, output_ims=True):
     """
     Produce Segmented Outputs for images
     @param eval_data_loader: a SegList that outputs test images. Should have Batch size of 1
     @param model: a trained DRNSeg model
-    @param num_classes: the number of classes in the dataset
-    @param output_dir: The directory where outputs will be saved
-    @param has_gt: Should be True if the dataloader also has ground truth segmentations
-    @param save_vis: Determines whether the outputs will be saved
     @param output_ims: Determines whether the function will return the segmented images
     @return: tuple of (mAP, outputs, road_mask), where mAP is a float and outputs is a list of segmented images.
                 if has_gt is false, mAP will be replaced with None. If output_ims=False,
@@ -250,7 +96,6 @@ def test(eval_data_loader, model, num_classes,
     batch_time = AverageMeter()
     data_time = AverageMeter()
     end = time.time()
-    hist = np.zeros((num_classes, num_classes))
     output = []
     masks = []
     inputs = []
@@ -268,180 +113,7 @@ def test(eval_data_loader, model, num_classes,
             output.extend(generate_colorful_images(pred))
             masks.extend(generate_road_mask(pred))
             inputs.append(rgb_image[0])
-
-        if save_vis:
-            save_output_images(pred, name, output_dir)
-            save_colorful_images(
-                pred, name, output_dir + '_color',
-                TRIPLET_PALETTE if num_classes == 3 else CITYSCAPE_PALETTE)
-        if has_gt:
-            label = label.numpy()
-            hist += fast_hist(pred.flatten(), label.flatten(), num_classes)
-            logger.info('===> mAP {mAP:.3f}'.format(
-                mAP=round(np.nanmean(per_class_iu(hist)) * 100, 2)))
         end = time.time()
-        logger.info('Eval: [{0}/{1}]\t'
-                    'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                    'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                    .format(iter, len(eval_data_loader), batch_time=batch_time,
-                            data_time=data_time))
-    if has_gt: #val
-        ious = per_class_iu(hist) * 100
-        logger.info(' '.join('{:.03f}'.format(i) for i in ious))
-        return round(np.nanmean(ious), 2), output_ims
     # The first class is the road
     return None, output, masks, inputs
 
-
-def resize_4d_tensor(tensor, width, height):
-    tensor_cpu = tensor.cpu().numpy()
-    if tensor.size(2) == height and tensor.size(3) == width:
-        return tensor_cpu
-    out_size = (tensor.size(0), tensor.size(1), height, width)
-    out = np.empty(out_size, dtype=np.float32)
-
-    def resize_one(i, j):
-        out[i, j] = np.array(
-            Image.fromarray(tensor_cpu[i, j]).resize(
-                (width, height), Image.BILINEAR))
-
-    def resize_channel(j):
-        for i in range(tensor.size(0)):
-            out[i, j] = np.array(
-                Image.fromarray(tensor_cpu[i, j]).resize(
-                    (width, height), Image.BILINEAR))
-
-    # workers = [threading.Thread(target=resize_one, args=(i, j))
-    #            for i in range(tensor.size(0)) for j in range(tensor.size(1))]
-
-    workers = [threading.Thread(target=resize_channel, args=(j,))
-               for j in range(tensor.size(1))]
-    for w in workers:
-        w.start()
-    for w in workers:
-        w.join()
-    # for i in range(tensor.size(0)):
-    #     for j in range(tensor.size(1)):
-    #         out[i, j] = np.array(
-    #             Image.fromarray(tensor_cpu[i, j]).resize(
-    #                 (w, h), Image.BILINEAR))
-    # out = tensor.new().resize_(*out.shape).copy_(torch.from_numpy(out))
-    return out
-
-
-def test_seg(args):
-    batch_size = 1
-    num_workers = 2
-    # Set to model path
-    MODEL_PATH = "/drn_d_22_cityscapes.pth"
-    phase = "test"
-
-    for k, v in args.__dict__.items():
-        print(k, ':', v)
-
-    single_model = DRNSeg("drn_d_22", 19, pretrained_model=None,
-                          pretrained=False)
-
-    single_model.load_state_dict(torch.load(MODEL_PATH))
-    model = torch.nn.DataParallel(single_model).cuda()
-
-    # data_dir = args.data_dir
-    # info = json.load(open(join(data_dir, 'info.json'), 'r'))
-    # Cityscapes Statistics
-    info = {"std": [0.1829540508368939, 0.18656561047509476, 0.18447508988480435], "mean": [0.29010095242892997, 0.32808144844279574, 0.28696394422942517]}
-    normalize = transforms.Normalize(mean=info['mean'], std=info['std'])
-    # Build Dataset
-    # TODO: Replace with appropriate dataloader
-    dataset = SegList(data_dir, phase, transforms.Compose([
-        transforms.ToTensor(),
-        normalize,
-    ]), list_dir=args.list_dir, out_name=True)
-
-    test_loader = torch.utils.data.DataLoader(
-        dataset,
-        batch_size=batch_size, shuffle=False, num_workers=num_workers,
-        pin_memory=False
-    )
-
-    cudnn.benchmark = True
-
-    # optionally resume from a checkpoint
-    start_epoch = 0
-
-    out_dir = '{}_{:03d}_{}'.format("drn_d_22, 19", start_epoch, phase)
-
-    # Get Outputs
-    # TODO: change parameters as necessary, and handle outputs
-    _, outputs = test(test_loader, model, 19, save_vis=False,
-               has_gt=False, output_ims=True)
-
-
-def parse_args():
-    # Training settings
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('cmd', choices=['train', 'test'])
-    parser.add_argument('-d', '--data-dir', default=None, required=True)
-    parser.add_argument('-l', '--list-dir', default=None,
-                        help='List dir to look for train_images.txt etc. '
-                             'It is the same with --data-dir if not set.')
-    parser.add_argument('-c', '--classes', default=0, type=int)
-    parser.add_argument('-s', '--crop-size', default=0, type=int)
-    parser.add_argument('--step', type=int, default=200)
-    parser.add_argument('--arch')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
-    parser.add_argument('--lr-mode', type=str, default='step')
-    parser.add_argument('--momentum', type=float, default=0.9, metavar='M',
-                        help='SGD momentum (default: 0.9)')
-    parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
-                        metavar='W', help='weight decay (default: 1e-4)')
-    parser.add_argument('-e', '--evaluate', dest='evaluate',
-                        action='store_true',
-                        help='evaluate model on validation set')
-    parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                        help='path to latest checkpoint (default: none)')
-    parser.add_argument('--pretrained', dest='pretrained',
-                        default='', type=str, metavar='PATH',
-                        help='use pre-trained model')
-    parser.add_argument('--save_path', default='', type=str, metavar='PATH',
-                        help='output path for training checkpoints')
-    parser.add_argument('--save_iter', default=1, type=int,
-                        help='number of training iterations between'
-                             'checkpoint history saves')
-    parser.add_argument('-j', '--workers', type=int, default=8)
-    parser.add_argument('--load-release', dest='load_rel', default=None)
-    parser.add_argument('--phase', default='val')
-    parser.add_argument('--random-scale', default=0, type=float)
-    parser.add_argument('--random-rotate', default=0, type=int)
-    parser.add_argument('--bn-sync', action='store_true')
-    parser.add_argument('--ms', action='store_true',
-                        help='Turn on multi-scale testing')
-    parser.add_argument('--with-gt', action='store_true')
-    parser.add_argument('--test-suffix', default='', type=str)
-    args = parser.parse_args()
-
-    assert args.classes > 0
-
-    print(' '.join(sys.argv))
-    print(args)
-
-    if args.bn_sync:
-        drn.BatchNorm = batchnormsync.BatchNormSync
-
-    return args
-
-
-def main():
-    args = parse_args()
-    if args.cmd == 'train':
-        train_seg(args)
-    elif args.cmd == 'test':
-        test_seg(args)
-
-
-if __name__ == '__main__':
-    main()
